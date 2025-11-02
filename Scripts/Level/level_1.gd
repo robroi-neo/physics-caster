79b7b40befa719a0587134c5286b90f59dbd6f9b
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2025-11-02 20:58:01 +0800

    feat: add trigger on bridge

--- Scripts/Level/level_1.gd
+++ Scripts/Level/level_1.gd
@@ -6,3 +6,9 @@ func _ready() -> void:
 	# Connect Triggers and Targets Here
 	$Triggers/Trigger_1.activated.connect($Triggers_Platform/Platform_1.activate)
 	$Triggers/Trigger_2.activated.connect($Triggers_Platform/Platform_2.activate)
+	# Trigger_3 activates multiple platforms
+	$Triggers/Trigger_3.activated.connect(showBridge)
+	
+func showBridge() -> void:
+	$Triggers_Platform/Platform_Bridge.visible = true
+	$Triggers_Platform/Platform_Bridge.set_collision_enabled(true)
