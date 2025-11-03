f7504ea39257b38a97eaa8dc701b5322eb1aa7df
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2025-11-03 14:42:57 +0800

    feat: add killzone functionality

---
+++ Scripts/Level/killzone.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var x:int
+@export var y:int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.position = Vector2(x,y)
+	pass # Replace with function body.
